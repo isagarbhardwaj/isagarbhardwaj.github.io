e107d24bad4839486f31256b7687ebc81c4bb50f
Sagar Bhardwaj <39807664+isagarbhardwaj@users.noreply.github.com>
2024-07-24 17:04:02 +0530

    Final layout changes

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,36 +5,85 @@ import { SiMagento } from "react-icons/si"
 import { SiMysql } from "react-icons/si"
 import { DiPhp } from "react-icons/di"
 import { TbBrandTailwind } from "react-icons/tb"
+import { animate, motion, transform } from "framer-motion"
 
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    }
+})
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">
+        <motion.h2 
+            whileInView={{y: 0, opacity: 1}} 
+            initial={{y: -100, opacity: 0}} 
+            transition={{duration: 1.5}}
+            className="my-20 text-center text-4xl">
             Technologies
-        </h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.h2>
+        <motion.div 
+            whileInView={{x: 0, opacity: 1}} 
+            initial={{x: -100, opacity: 0}} 
+            transition={{duration: 1.5}} 
+            className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <DiPhp className="text-7xl text-white-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiMagento className="text-7xl text-orange-600" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <TbBrandJavascript className="text-7xl text-yellow-600" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <TbBrandTailwind className="text-7xl text-sky-700" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiMysql className="text-7xl text-white-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(4.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <TbBrandGraphql className="text-7xl text-pink-600" />
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,38 +3,52 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack Adobe Commerce developer with a knack for crafting robust and scalable e-commerce web applications. With 6 years of hands-on experience, I have honed my skills in Adobe Commerce aka Magento2, PHP, JavaScript, jQuery, MySQL, AJAX, JSON and Web API. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    year: "JULY 2023 - Present",
+    role: "Senior Engineer",
+    company: "GlobalLogic, Gurugram",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
+    year: "JAN 2022 - JUNE 2023",
+    role: "Senior Software Engineer",
+    company: "LTIMindtree, Noida",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
+    year: "DEC 2020 - JAN 2022",
+    role: "Software Engineer",
+    company: "Vamani Overseas, Faridabad",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    technologies: ["Magento2", "ERP Integrations", "PHP", "MySQL"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
+    year: "AUG 2020 - DEC 2020",
+    role: "Software Developer",
+    company: "Solveda Software India Pvt Ltd, Gurugram",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    technologies: ["Magento2", "PHP", "MySQL"],
+  },
+  {
+    year: "FEB 2020 - APR 2020",
+    role: "Software Engineer",
+    company: "Venture7, Faridabad",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Magento2", "MySQL", "Jquery"],
+  },
+  {
+    year: "NOV 2017 - FEB 2020",
+    role: "Web Developer",
+    company: "Excellence Technologies Pvt Ltd, Noida",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["PHP", "Magento2", "Jquery", "MySQL"],
   },
 ];
 
@@ -70,7 +84,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "269/7 Village Fatehpur Chandila, Faridabad, HR - 121001 ",
+  phoneNo: "+91 9717822270 ",
+  email: "dev.sagarbhardwaj@gmail.com",
 };
